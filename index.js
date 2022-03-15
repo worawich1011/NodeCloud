3c016b15865754bdc6005387853d0a8f1e5829ec
worawich1011 <worawich1011@gmail.com>
2022-03-15 15:35:21 +0700

    change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! On Cloud test');
+  res.send('Hello World! On Cloud');
 });
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`);
